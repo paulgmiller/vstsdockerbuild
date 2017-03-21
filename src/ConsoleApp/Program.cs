552b1636cbc193a27a11dfacd469c1e27ac7567c
Jeff Kluge <jeffkl@microsoft.com>
2017-03-21 14:09:23 -0700

    Initial commit

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JObject jObject = JObject.Parse(" { 'Success': true } ");
+
+            Console.WriteLine($"Success: {jObject["Success"]}");
+        }
+    }
+}
