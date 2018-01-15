80b881e1aa79b5e03e18b33abd653fe5fda0ab82
Paul Miller <paul.miller@gmail.com>
2018-01-14 22:10:28 -0800

    try and get docker version from docker container not working

--- Controllers/DockerController.cs
+++ Controllers/DockerController.cs
@@ -3,25 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Docker.DotNet;
 
 namespace vstsdockerbuild.Controllers
 {
-    [Route("probes")]
-    public class KeepaliveController : Controller
+    [Route("docker")]
+    public class DockerController : Controller
     {
         // GET api/values
         [HttpGet]
-        [Route("alive")]
-        public IActionResult Alive()
+        [Route("version")]
+        public async Task<IActionResult> Version()
         {
-            return Ok("ALIVE");
+            var client = new DockerClientConfiguration(new Uri("http://localhost:2375"))
+                                     .CreateClient();
+            var version = await client.System.GetVersionAsync();
+            return Ok(version.Version);
         }
 
-        [HttpGet]
-        [Route("ready")]
-        public IActionResult Ready()
-        {
-            return Ok("READY");
-        }
+       
     }
 }
